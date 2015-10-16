5a30065abb6c0ec96dc4bb978743a2c725ec9eb5
Ben Southgate <bsouthga@gmail.com>
2015-10-16 11:10:01 -0400

    data for demo success

--- scripts/enron.py
+++ scripts/enron.py
@@ -4,36 +4,39 @@
 # http://mongodb-enron-email.s3-website-us-east-1.amazonaws.com/
 # and export the messages collection to a csv
 #
-import csv
 import json
-import pymongo as pm
+import time
+from pymongo import MongoClient
 from dateutil.parser import parse
 
-def employees(filename='./employees.txt'):
-  with open(filename, 'r') as intext:
-    with open(filename.replace('txt', 'json'), 'w') as outjson:
-      out = []
-      for line in intext:
-        out.append(line.split("\t")[0].strip() + "@enron.com")
-      json.dump(out, outjson)
+# Prep emails for use in gak.EventRank
+def mongoClean(collection='messages'):
+  client = MongoClient()
+  db = client['__fakeenron']
+  out = []
+  count = 0
+  total = float(db[collection].count())
+  with open('./mongo-enron.json', 'w') as outjson:
+    for message in db[collection].find():
+      count += 1
+      if (count < 10):
+        print(message['headers']['Date'])
+        print(parse(message['headers']['Date']).timetuple())
+        print(time.mktime(parse(message['headers']['Date']).timetuple()))
+      if (count % 1000) == 0:
+        workdone = count / total
+        print("\rProgress: [{0:50s}] {1:.1f}%".format('#' * int(workdone * 50), workdone*100), end="", flush=True)
 
+      getSet = lambda k : set(x.strip() for x in message['headers'][k].split(',')) if k in message['headers'] else set()
+      record = {
+        "to" : [x for x in (getSet('To') | getSet('Bcc') | getSet('Cc')) if x],
+        "from" : message['headers']['From'],
+        "time" : time.mktime(parse(message['headers']['Date']).timetuple())
+      }
+      out.append(record)
 
-def mongoClean(filename='./mongo-enron.csv'):
-  with open(filename, "r") as csv_file:
-    csv_iterator = csv.DictReader(csv_file)
-    out = []
-    with open('./mongo-enron.json', 'w') as outjson:
-      for row in csv_iterator:
-        getSet = lambda x : set(x.strip() for x in row['headers.' + x].split(','))
-        record = {
-          "to" : [x for x in (getSet('To') | getSet('Bcc') | getSet('Cc')) if x],
-          "from" : row['headers.From'],
-          "time" : int(parse(row['headers.Date']).strftime('%s'))
-        }
-        out.append(record)
-      json.dump(sorted(out, lambda x, y: x['time'] - y['time']), outjson)
+    json.dump(sorted(out, key=lambda x: x['time']), outjson)
 
 
 if __name__ == '__main__':
-  #mongoClean()
-  employees()
+  mongoClean()
--- scripts/rank.js
+++ scripts/rank.js
@@ -1,1 +1,45 @@
-var EventRank = require('gak').EventRank;
+var EventRank   = require('gak').EventRank;
+var ProgressBar = require('progress');
+var _           = require('lodash');
+var d3          = require('d3');
+var moment      = require('moment');
+
+var maxYear = new Date('2001');
+var n = 250;
+var oneDay = 24 * 60 * 60;
+
+console.log('Loading enron emails...');
+var emails = require('./mongo-enron.json').filter(function(e) {
+  return moment(e.time*1000) < maxYear;
+});
+
+console.log('Getting correspondent set...');
+
+var correspondents = require('./correspondents.json');
+
+var R = new EventRank({
+  correspondents: correspondents,
+  g: oneDay,
+  h: oneDay,
+  model:'reply'
+});
+
+var bar = new ProgressBar('Stepping... [:bar] :percent :etas', { total: emails.length, width: 80 });
+
+for (var i = 0, l = emails.length; i < l; i++) {
+  var email = emails[i];
+  if (email.to && email.to.length) {
+    R.step(email);
+  }
+  bar.tick();
+}
+
+var catchUpBar = new ProgressBar('Catching up... [:bar] :percent :etas', { total: correspondents.length, width: 80 });
+
+for (var i=0, l=correspondents.length; i < l; i++) {
+  R.catchUp(correspondents[i]);
+  catchUpBar.tick();
+}
+
+console.log('Getting top ' + n + ' ranks...');
+console.log(_.map(R.top(n), function(v) { return v.id; }));
